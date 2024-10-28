56bca90cb13523376a4f774ee1b936ea03ca1ec8
sadanadnan <sa2239@kent.ac.uk>
2024-10-28 04:42:10 +0000

    3rd one

--- ListManipulation.py
+++ ListManipulation.py
@@ -1,1 +1,10 @@
 ### Part Three -- your code goes here. 
+numbers = [3, 1, 4, 1, 5, 9, 2, 6, 5, 3]
+
+numbers.sort()
+
+numbers = [num for num in numbers if num != 1]
+
+numbers.extend([7, 8])
+
+print("Final List:", numbers)
