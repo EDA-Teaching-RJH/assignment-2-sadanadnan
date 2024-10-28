066b0c080ca1b1cf4baf11a5387595629e0ae1cb
sadanadnan <sa2239@kent.ac.uk>
2024-10-28 04:33:33 +0000

    while loop

--- WhileLoops.py
+++ WhileLoops.py
@@ -1,1 +1,17 @@
 ### Part Two -- your code goes here. 
+print("welcome to guessing game")
+import random
+print("pick a number b/w 1-100")
+number = random.randint(1, 100)
+
+while True:
+    val = int(input("your guess is:"))
+    if number > val:
+        print("you guessed a number too low")
+
+    elif number < val:
+        print("you guess a number too high")
+    
+    else: 
+        print(val, "is the right number!!! good job, you guessed it!!")
+        break
