73a151e9a788caaff2d0830c0f6cc2887c86251a
sadanadnan <sa2239@kent.ac.uk>
2024-10-28 04:49:16 +0000

    last code

--- AllTogether.py
+++ AllTogether.py
@@ -1,1 +1,15 @@
 ### Part Four -- your code goes here. 
+import random
+
+numbers = [random.randint(1, 100) for _ in range(10)]
+print("First List:", numbers)
+
+index = 0
+while index < len(numbers):
+    if numbers[index] % 2 == 0: 
+        numbers.pop(index)  
+    else:
+        index += 1 
+
+numbers.sort()
+print("Final List:", numbers)
